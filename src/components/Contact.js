f8bf39733231d7627530b22e00383782b9f65407
3ndrius <andrzejo1991@gmail.com>
2018-10-10 20:50:12 +0200

    Adding router and navlink to app component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,15 @@
 import React  from 'react';
 
 
-const About = () =>{
+const Contact = () =>{
     return(
         <div className="row">
             <div className="col-md-12">
-                <h2>About</h2>
+                <h2>Contact</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore laborum nulla explicabo a, voluptas voluptatem quod perferendis ex ipsum laudantium doloribus itaque incidunt fugiat molestiae odit qui, provident fugit. Ab.</p>
             </div>
         </div>
     )
 }
 
-export default About;
+export default Contact;
