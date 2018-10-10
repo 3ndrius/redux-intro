d63fac03dc8e81def4e30ef3d790fbf1e6227658
3ndrius <andrzejo1991@gmail.com>
2018-10-10 20:22:25 +0200

    Adding old filed

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+
+const About = () =>{
+    return(
+        <div className="row">
+            <div className="col-md-12">
+                <h2>About</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore laborum nulla explicabo a, voluptas voluptatem quod perferendis ex ipsum laudantium doloribus itaque incidunt fugiat molestiae odit qui, provident fugit. Ab.</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+
+const Home = () =>{
+    return(
+        <div className="row">
+            <div className="col-md-12">
+                <h2>Home</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore laborum nulla explicabo a, voluptas voluptatem quod perferendis ex ipsum laudantium doloribus itaque incidunt fugiat molestiae odit qui, provident fugit. Ab.</p>
+            </div>
+        </div>
+    )
+}
